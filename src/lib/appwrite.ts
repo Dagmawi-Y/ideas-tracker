6241d1160295ec7e5a46511f8e456b1ce50451d3
Dagmawi-Y <dagmawi.inbox@gmail.com>
2025-01-09 15:30:20 +0300

    Refactor Appwrite client configuration to use environment variables for endpoint and project ID

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,7 +1,8 @@
 import { Client, Databases, Account } from 'appwrite';
+import { PUBLIC_APPWRITE_ENDPOINT, PUBLIC_APPWRITE_PROJECT_ID } from '$env/static/public';
 
 const client = new Client();
-client.setEndpoint('https://cloud.appwrite.io/v1').setProject('677fb4260027182dcecb');
+client.setEndpoint(PUBLIC_APPWRITE_ENDPOINT).setProject(PUBLIC_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
